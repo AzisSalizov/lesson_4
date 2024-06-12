35a3cdd2e48ed14dc68728bd43f0da87bb98fc5c
Azis <azissalizov@gmail.com>
2024-06-12 15:57:02 +0600

    fixed a bug boss does damage to all heroes

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import java.util.Random;
 
 public class Main {
-    public static int bossHealth = 700;
+    public static int bossHealth = 1100;
     public static int bossDamage = 50;
     public static String bossDefence;
-    public static int[] heroesHealth = {290, 270, 250, 230, 550, 215};
+    public static int[] heroesHealth = {290, 270, 250, 230, 550, 215 };
     public static int[] heroesDamage = {35, 25, 15, 0, 8, 12};
-    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Lucky"};
+    public static String[] heroesAttackType = {"Physical", "Magical", "Kinetic", "Medic", "Golem", "Lucky" };
     public static int roundNumber = 0;
 
     public static void main(String[] args) {
@@ -31,7 +31,7 @@ public class Main {
                     Random random = new Random();
                     int medicHealed = random.nextInt(50) + 1;
                     heroesHealth[i] += medicHealed;
-                    System.out.println("Medic healed " + heroesAttackType[i] + "for " + medicHealed + " health!!!");
+                    System.out.println("Medic healed " + heroesAttackType[i] + " for " + medicHealed + " health!!!");
                     break;
                 }
             }
@@ -116,9 +116,8 @@ public class Main {
         for (int i = 0; i < heroesHealth.length; i++) {
             if (heroesHealth[i] > 0) {
                 Random random = new Random();
-                boolean isDodged = random.nextInt(100) < 10;
-                if (i == luckyIndex && isDodged) {
-                    System.out.println(heroesAttackType[i] + " is dodged BOSS attack!!!");
+                boolean isDodged = random.nextInt(100) < 20;
+                if (i != luckyIndex || !isDodged) {
                     if (golemIndex != -1 && heroesHealth[golemIndex] > 0 && i != golemIndex) {
                         int absorbDamage = bossDamage / 5;
                         heroesHealth[golemIndex] -= absorbDamage;
@@ -129,6 +128,8 @@ public class Main {
                     } else {
                         heroesHealth[i] -= bossDamage;
                     }
+                } else {
+                    System.out.println(heroesAttackType[i] + " is dodged BOSS attack!!!");
                 }
             }
         }
